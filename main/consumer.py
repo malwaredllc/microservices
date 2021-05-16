3b09020b1f1eeab72f349d762271bbbbbc505908
malwaredllc <30509968+malwaredllc@users.noreply.github.com>
2021-05-16 14:05:04 -0600

    fix consumer

--- main/consumer.py
+++ main/consumer.py
@@ -7,7 +7,7 @@ channel = connection.channel()
 channel.queue_declare(queue='main')
 
 def callback(ch, method, properties, body):
-    print("received in admin:", body)
+    print("received in main:", body)
 
 channel.basic_consume(queue='main', on_message_callback=callback)
 
