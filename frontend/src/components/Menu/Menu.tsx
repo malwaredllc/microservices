b156cd08b123f31945d6f47841a13bb25fe9b1ae
malwaredllc <30509968+malwaredllc@users.noreply.github.com>
2021-05-19 13:45:33 -0600

    begin work on react frontend

---
+++ frontend/src/components/Menu/Menu.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Menu = () => {
+    return (
+        <div className="menu">
+            <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+                <div className="position-sticky pt-3">
+                    <ul className="nav flex-column">
+                        <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
+                            Dashboard
+                        </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default Menu;
