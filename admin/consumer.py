793aee45bfc26dbe7c0154e352919be95fffe1fa
malwaredllc <30509968+malwaredllc@users.noreply.github.com>
2021-05-18 21:06:46 -0600

    implementing like feature

--- admin/consumer.py
+++ admin/consumer.py
@@ -1,4 +1,11 @@
-import pika
+import pika, json, os, django
+
+# django settings
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', "admin.settings")
+django.setup()
+
+from products.models import Product
+
 
 RABBIT_MQ_URL = 'amqps://gifcipsg:mRdHfRUPHoO8Z3EkSbwv34Tk0u0M1tpm@baboon.rmq.cloudamqp.com/gifcipsg/%2F'
 params = pika.URLParameters(RABBIT_MQ_URL)
@@ -7,7 +14,11 @@ channel = connection.channel()
 channel.queue_declare(queue='admin')
 
 def callback(ch, method, properties, body):
-    print("received in admin:", body)
+    id = json.loads(body)
+    product = Product.objects.get(id=id)
+    product.likes = product.likes + 1
+    product.save()
+    print('Product likes increased')
 
 channel.basic_consume(queue='admin', on_message_callback=callback, auto_ack=True)
 
--- main/main.py
+++ main/main.py
@@ -1,9 +1,11 @@
 import requests
 from dataclasses import dataclass
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from sqlalchemy import UniqueConstraint
+
+from producer import publish
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:root@db/main'
@@ -23,6 +25,10 @@ class Product(db.Model):
 
 @dataclass
 class ProductUser(db.Model):
+    id: int
+    user_id: int
+    product_id: str
+
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer)
     product_id = db.Column(db.Integer)
@@ -33,10 +39,26 @@ class ProductUser(db.Model):
 def index():
     return jsonify(Product.query.all())
 
-@app.route('/api/products<int:id>/like', methods=['POST'])
+@app.route('/api/product_users')
+def get_product_users():
+    return jsonify(ProductUser.query.all())
+
+@app.route('/api/products/<int:id>/like', methods=['POST'])
 def like(id):
-    req = requests.get('http://localhost:8000/api/user')
-    return req.json()
+    req = requests.get('http://docker.for.mac.localhost:8000/api/user')
+    try:
+        user_id = req.json()['id']
+        product_user = ProductUser(user_id=user_id, product_id=id)
+        db.session.add(product_user)
+        db.session.commit()
+
+        # publish event to rabbitmq
+        publish('product_liked', id)
+    except Exception as e:
+        abort(400, str(e))
+    return jsonify({
+        'message': 'success'
+    })
     
 
 if __name__ == '__main__':
